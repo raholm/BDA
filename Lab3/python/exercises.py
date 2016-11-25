feb556c6d3b5ffdf5adefb5854529431fe15c35b
raholm <rasmus.holm723@gmail.com>
2016-11-25 13:46:14 +0100

    Updated gitignore and created a skeleton for the lab.

---
+++ Lab3/python/exercises.py
@@ -0,0 +1,30 @@
+def exercise01():
+    temperatures = sc.textFile("../data/temperature-readings.csv")
+
+def exercise02():
+    pass
+
+def exercise03():
+    pass
+
+def exercise04():
+    pass
+
+def exercise05():
+    pass
+
+def exercise06():
+    pass
+
+def main():
+    exercise01()
+    exercise02()
+    exercise03()
+    exercise04()
+    exercise05()
+    exercise06()
+
+if __name__ == "__main__":
+    main()
+
+main()
