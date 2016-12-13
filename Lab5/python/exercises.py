1b9727445105f36ecad534bfd0a2931d5b5f713e
raholm <rasmus.holm723@gmail.com>
2016-12-13 22:33:04 +0100

    Minor adjustments.

--- Lab5/python/exercises.py
+++ Lab5/python/exercises.py
@@ -31,48 +31,38 @@ def kernel_model():
 
     a = 58.4274 # Up to you
     b = 14.826 # Up to you
-    date = "2013-07-04" # Up to you
 
-    date = datetime.date(datetime.strptime("2013-07-04", "%Y-%m-%d"))
-    time = datetime.time(datetime.strptime("08:00:00", "%H:%M:%S"))
+    pred_date = datetime.date(datetime.strptime("2013-07-04", "%Y-%m-%d"))
+    pred_time = datetime.time(datetime.strptime("08:00:00", "%H:%M:%S"))
+    pred_datetime = datetime.combine(pred_date, pred_time)
 
     stations = sc.textFile("../data/stations.csv")
     temps = sc.textFile("../data/temps50k.csv")
 
     stations = stations.map(lambda line: line.split(",")) \
-                       .map(lambda obs: (int(obs[0]), (float(obs[3]), float(obs[4]))))
-
-    stations.collect()
+                       .map(lambda obs: (int(obs[0]),
+                                         (float(obs[3]),
+                                          float(obs[4]))))
 
     temps = temps.filter(lambda line: len(line) > 0) \
                  .map(lambda line: line.split(",")) \
-                 .map(lambda obs: (int(obs[0]), (datetime.date(datetime.strptime(obs[1], "%Y-%m-%d")),
-                                                 datetime.time(datetime.strptime(obs[2], "%H:%M:%S")),
-                                                 float(obs[3]))))
+                 .map(lambda obs: (int(obs[0]),
+                                   (datetime.date(datetime.strptime(obs[1], "%Y-%m-%d")),
+                                    datetime.time(datetime.strptime(obs[2], "%H:%M:%S")),
+                                    float(obs[3]))))
 
-    temps.collect()
-    # print(temps.take(5))
-    # print(stations.take(5))
+    combined_data = stations.join(temps)
+    combined_data.cache()
 
-    combined = stations.join(temps)
-    combined.cache()
+    combined_data = combined_data.filter(lambda obs: filter_date(x=obs, dt=pred_datetime))
 
-
-    # data.filter(lambda obs: date_filter(obs, pred_date, pred_time))
-    combined = combined.filter(lambda obs: filter_date(x=obs, date=date, time=time))
+    print(combined_data.count())
 
 
-    print(date)
-    print(time)
-    print(datetime.combine(date, time))
-    # print(combined.take(5))
-
-
-def filter_date(x, date, time):
-    merged_pred = datetime.combine(date, time)
+def filter_date(x, dt):
+    # x - (station, ((lat, long), (date, time, temp)))
     merged_raw = datetime.combine(x[1][1][0], x[1][1][1])
-    # (station, ((long, lat), (date, time, temp)))
-    return merged_raw <= merged_pred
+    return merged_raw <= dt
 
 def main():
     kernel_model()
